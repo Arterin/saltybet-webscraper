b5b60a64caf431a742ee34a3b51b61edc953dd73
Vivian Luxon <arterinv@gmail.com>
2021-04-26 22:33:43 +1200

    Finished main functionality of scraping function.

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 function scrapeSaltybet() {
-    // get saltybet html
+
+    // Get saltybet html.
     axios.get('https://www.saltybet.com/shaker').then((response) => {
-        // load html into cheerio
-        const $ = cheerio.load(response.data);
-        // get div I want
-        let div = $("#compendiumleft > div");
-        div = div.text();
-        div = div.trimLeft();
-        let lastExclamation = div.indexOf('!') + 1;
-        div = div.slice(0, lastExclamation);
-        console.log(div);
+        // Load html into cheerio.
+        const html = cheerio.load(response.data);
+        // Get tournament countdown text from the div.
+        let saltyText = html("#compendiumleft > div");
+        saltyText = saltyText.text();
+        // Format text.
+        saltyText = saltyText.trimLeft();
+        const lastExclamation = saltyText.indexOf('!') + 1;
+        saltyText = saltyText.slice(0, lastExclamation);
+        console.log(saltyText);
+        // TODO Add tweet function and call it here with saltyText as input.
     })
+    // TODO Add fail case.
+    // TODO Add timeout fail case.
 }
+
+scrapeSaltybet();
